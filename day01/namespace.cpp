d1e840d2fce7407afe37f8a065037fbff31b531d
panxinjia <m15132582590@163.com>
2024-03-17 11:58:06 +0800

    a

--- day01/namespace.cpp
+++ day01/namespace.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+namespace zs {
+  void radius() {
+    cout << "zs:radius" << endl;
+  }
+}
+
+namespace lisi {
+  void radius() {
+    cout << "lisi:radius" << endl;
+  }
+}
+
+namespace zs {
+  int add(int a, int b) {
+    return a + b;
+  }
+}
+
+struct student {
+  char name[100];
+  int number;
+};
 
 int main(int args, const char* argv[]) {
+  // namespace
+  zs::radius();
+  lisi::radius();
 
+  int sum = zs::add(3, 4);
+  cout << sum << endl;
+
+  // 标准输入输出 std 标准库命名空间, cout end: cpp中标准iostream对象实例
+  std::cout << "一起学习cpp" << std::endl;
   return 0;
 }
