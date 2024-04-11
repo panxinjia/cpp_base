9ee3935bbf17525e7a34fdffcc54b1310647b6ab
panxinjia <m15132582590@163.com>
2024-04-11 16:14:55 +0800

    commit

--- muke_c/test/include/search.h
+++ muke_c/test/include/search.h
@@ -3,12 +3,10 @@
 
 #include <stdio.h>
 
-// 线性查找
 int LinearSearchBase(int arr[], int key, int len);
 void* LinearSearch(void* base, void* key, int len, size_t size,
                    int (*cmpFunc)(void*, void*));
 
-// 二分查找
 int BinarySearchBase(int arr[], int key, int len);
 void* BinarySearch(void* lp, void* rp, void* key, size_t size,
                    int (*cmpFunc)(void*, void*));
